d7e08aeb870076e6aadcf5e0ccd25cb84cfd9674
Ajie Pangestu <paangestu@gmail.com>
2021-08-26 21:58:03 +0700

    add safety init database

--- database/database.go
+++ database/database.go
@@ -14,19 +14,21 @@ var DB *sql.DB
 func Connect() error {
 	var err error
 	// DSN format = username:password@protocol(address)/dbname?param=value
-	dsn := fmt.Sprintf("%s:%s@%s(%s:%s)/%s",
-		config.Config("DB_USER"),
-		config.Config("DB_PASSWORD"),
-		config.Config("DB_PROTOCOL"),
-		config.Config("DB_HOST"),
-		config.Config("DB_PORT"),
-		config.Config("DB_NAME"),
+	DB, err = sql.Open("mysql",
+		fmt.Sprintf("%s:%s@%s(%s:%s)/%s",
+			config.Config("DB_USER"),
+			config.Config("DB_PASSWORD"),
+			config.Config("DB_PROTOCOL"),
+			config.Config("DB_HOST"),
+			config.Config("DB_PORT"),
+			config.Config("DB_NAME"),
+		),
 	)
-	DB, err = sql.Open("mysql", dsn)
 	if err = DB.Ping(); err != nil {
 		return err
 	}
-	InitDatabase()
-	fmt.Println("Connection Opened to Database")
+	if err := InitDatabase(); err != nil {
+		return err
+	}
 	return nil
 }
